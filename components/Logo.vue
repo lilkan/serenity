183e5a42313272d517c72205692c0b7d5bb8e36b
german <doooomen@gmail.com>
2020-09-03 16:54:16 +0300

    sds

---
+++ components/Logo.vue
@@ -0,0 +1,29 @@
+<template>
+ <h1>hello</h1>
+
+</template>
+<script>
+
+  export default {
+    data(){
+      return{
+        msg: ''
+      }
+    },
+    async created(){
+      try {
+        const res = await this.$axios.$get("http://localhost:3000/posts");
+        console.log(res);
+        this.msg = res;
+        console.log(res.data)
+      } catch (e) {
+        console.error(e)
+      }
+    }
+  }
+</script>
+<style lang="scss" scoped>
+h1 {
+  font-size: 25px
+}
+</style>
